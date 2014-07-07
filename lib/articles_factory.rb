fbd6386eeb77c75f4ac830f1a29316afb30c75c6
Jean Kirchner <jean.kirchner@gmail.com>
2014-07-07 16:38:16 -0300

    String html from rss content

--- lib/articles_factory.rb
+++ lib/articles_factory.rb
@@ -4,7 +4,7 @@ class ArticlesFactory
   def create(rss)
     articles = []
     rss.items.map do |item|
-      article = Article.new(get_item_prop(item.link))
+      article = Article.new(strip_prop(item.link))
       gather_required_properties(article, item)
       gather_optional_properties(article, item, rss.rss_version)
       articles.push(article)
@@ -15,27 +15,32 @@ class ArticlesFactory
   private
 
   def gather_required_properties(article, rss_item)
-    article.title = get_item_prop(rss_item.title)
-    article.description  = get_item_prop(rss_item.description)
+    article.title = strip_prop(rss_item.title)
+    article.description  = strip_html(strip_prop(rss_item.description))
   end
 
   def gather_optional_properties(article, rss_item, rss_version)
     article.datePublished = get_date(rss_item.date, rss_version)
-    article.author = get_item_prop(rss_item.dc_creator)
-    article.articleBody = get_item_prop(rss_item.content_encoded)
+    article.author = strip_prop(rss_item.dc_creator)
+    article.articleBody = strip_html(strip_prop(rss_item.content_encoded))
     article.articleSection = rss_item.category if rss_item.respond_to?(:category)
-    article.language = get_item_prop(rss_item.dc_language)
+    article.language = strip_prop(rss_item.dc_language)
   end
 
-  def get_item_prop(prop)
+  def strip_prop(prop)
     prop.to_s.strip
   end
 
+  def strip_html(str)
+    re = /<("[^"]*"|'[^']*'|[^'">])*>/
+    str.gsub!(re, '')
+    str
+  end
+
   def get_date(date, rss_version)
-    striped_date = get_item_prop(date)
+    striped_date = strip_prop(date)
     if !striped_date.empty?
       rss_version == "2.0" ? DateTime.rfc2822(striped_date) : DateTime.iso8601(striped_date)
     end
   end
-
 end
--- spec/articles_factory_spec.rb
+++ spec/articles_factory_spec.rb
@@ -103,7 +103,7 @@ describe ArticlesFactory do
       end
 
       it 'should set the article description' do
-        expect(article.description).to eq('<p>The World Wide Web Foundation is pleased to announce the <a href="http://ocds.open-contracting.org/standard/">first release of the Open Contracting Data Standard (OCDS)</a>, which aims to enhance and promote disclosure and participation in public contracting, for broad consultation. In this release, we present &#8230;</p>')
+        expect(article.description).to eq('The World Wide Web Foundation is pleased to announce the first release of the Open Contracting Data Standard (OCDS), which aims to enhance and promote disclosure and participation in public contracting, for broad consultation. In this release, we present &#8230;')
       end
 
       it 'should set the article articleBody' do

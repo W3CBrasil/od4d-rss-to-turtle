54c5f0e51cab88d125350a55fd12c3426d52d1b2
Marcelo Vargas <marcelo.vargas90@gmail.com>
2014-07-07 19:10:27 +0000

    Adding support to date on rss 1.0

--- lib/articles_factory.rb
+++ lib/articles_factory.rb
@@ -6,7 +6,7 @@ class ArticlesFactory
     rss.items.map do |item|
       article = Article.new(get_item_prop(item.link))
       gather_required_properties(article, item)
-      gather_optional_properties(article, item)
+      gather_optional_properties(article, item, rss.rss_version)
       articles.push(article)
     end
     articles
@@ -19,24 +19,23 @@ class ArticlesFactory
     article.description  = get_item_prop(rss_item.description)
   end
 
-  def gather_optional_properties(article, rss_item)
-    article.datePublished = get_date(rss_item.pubDate) if rss_item.respond_to?(:pubDate)
-    article.author = get_item_prop(rss_item.dc_creator) if rss_item.respond_to?(:dc_creator)
-    article.articleBody = get_item_prop(rss_item.content_encoded) if rss_item.respond_to?(:content_encoded)
+  def gather_optional_properties(article, rss_item, rss_version)
+    article.datePublished = get_date(rss_item.date, rss_version)
+    article.author = get_item_prop(rss_item.dc_creator)
+    article.articleBody = get_item_prop(rss_item.content_encoded)
     article.articleSection = rss_item.category if rss_item.respond_to?(:category)
-    article.language = get_item_prop(rss_item.dc_language) if rss_item.respond_to?(:dc_language)
+    article.language = get_item_prop(rss_item.dc_language)
   end
 
   def get_item_prop(prop)
-    if (prop != nil) then
-        prop = prop.to_s.strip
-    end
-    prop
+    prop.to_s.strip unless prop.nil?
   end
 
-  def get_date(prop)
-    striped_prop = get_item_prop(prop)
-    DateTime.rfc2822(striped_prop) if !striped_prop.nil?
+  def get_date(date, rss_version)
+    striped_date = get_item_prop(date)
+    if !striped_date.nil?
+      rss_version == "2.0" ? DateTime.rfc2822(striped_date) : DateTime.iso8601(striped_date)
+    end
   end
 
 end
--- spec/articles_factory_spec.rb
+++ spec/articles_factory_spec.rb
@@ -3,7 +3,8 @@ require 'articles_factory'
 
 describe ArticlesFactory do
 
-  it "should create two articles from rss" do
+  context "given a valid rss version 2.0 with two items" do
+
     rss_string = <<-eos
         <rss version="2.0">
           <channel>
@@ -24,13 +25,15 @@ describe ArticlesFactory do
 
     rss = RSS::Parser.parse(rss_string)
 
-    articlesFactory = ArticlesFactory.new
-    articles = articlesFactory.create(rss)
+    it "should create two articles from rss" do
+      articlesFactory = ArticlesFactory.new
+      articles = articlesFactory.create(rss)
 
-    expect(articles.count).to eq(2)
+      expect(articles.count).to eq(2)
+    end
   end
 
-  context "given a valid rss with one item" do
+  context "given a valid rss version 2.0 with one item" do
 
     rss_string = <<-eos
         <rss xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/" version="2.0">
@@ -109,6 +112,57 @@ describe ArticlesFactory do
 
       it 'should set the article datePublished' do
         expect(article.datePublished).to eq(DateTime.new(2014,6,26,17,57,45,'+00:00'))
+      end
+
+    end
+
+  end
+
+  context "given a valid rss version 1.0 with one item" do
+    rss_string = <<-eos
+      <rdf:RDF xmlns="http://purl.org/rss/1.0/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">
+        <channel rdf:about="http://web.idrc.ca/en/ev-1-201-1-DO_TOPIC.html?from=rss">
+        <title>International Development Research Centre</title>
+        <link>http://web.idrc.ca/en/ev-1-201-1-DO_TOPIC.html?from=rss</link>
+        <description>
+          IDRC is a Canadian Crown corporation that works in close collaboration with researchers from the developing world in their search for the means to build healthier, more equitable, and more prosperous societies.
+        </description>
+          <items>
+            <rdf:Seq>
+              <rdf:li rdf:resource="http://www.idrc.ca/en/ev-149607-201-1-DO_TOPIC.html"/>
+            </rdf:Seq>
+          </items>
+        </channel>
+        <item rdf:about="http://www.idrc.ca/en/ev-149607-201-1-DO_TOPIC.html">
+          <dc:date>2011-07-28T18:21:06+00:00</dc:date>
+          <title>Lasting Impacts &#151; How IDRC-funded research has improved lives in the developing world</title>
+          <link>http://web.idrc.ca/en/ev-149607-201-1-DO_TOPIC.html?from=rss</link>
+          <description>IDRC marks its 40th anniversary with a look back at the lasting impacts of IDRC-supported research.</description>
+        </item>
+      </rdf:RDF>
+    eos
+
+    rss = RSS::Parser.parse(rss_string)
+
+    context "when creating an article from rss" do
+
+      articlesFactory = ArticlesFactory.new
+      article = articlesFactory.create(rss)[0]
+
+      it "should set the article title" do
+        expect(article.title).to eq("Lasting Impacts \u0097 How IDRC-funded research has improved lives in the developing world")
+      end
+
+      it 'should set the article url' do
+        expect(article.url).to eq("http://web.idrc.ca/en/ev-149607-201-1-DO_TOPIC.html?from=rss")
+      end
+
+      it 'should set the article description' do
+        expect(article.description).to eq('IDRC marks its 40th anniversary with a look back at the lasting impacts of IDRC-supported research.')
+      end
+
+      it 'should set the article datePublished' do
+        expect(article.datePublished).to eq(DateTime.new(2011,7,28,18,21,06,'+00:00'))
       end
 
     end

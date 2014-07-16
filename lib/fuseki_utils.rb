c8a70c387a88118ae80b2aa558b46d0e15ff9b2a
Jean Kirchner <jean.kirchner@gmail.com>
2014-07-16 16:04:20 -0300

    Refactoring script to load rss

---
+++ lib/fuseki_utils.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+require 'log'
+
+class FusekiUtils
+  HOST = 'localhost'
+  PORT = '3030'
+  PATH = '/articles/data?default'
+
+  def self.put(data)
+    begin
+      Logger.log(:info, "Loading data in to semantic repository")
+
+      headers = {}
+      headers["Content-Type"] = "text/turtle;charset=utf-8;charset=utf-8"
+      request = Net::HTTP::Post.new(PATH)
+      request.initialize_http_header(headers)
+      request.body = data
+      response = Net::HTTP.new(HOST, PORT).start {|http| http.request(request) }
+
+      if response.kind_of? Net::HTTPSuccess
+        Logger.log(:info, "Data loaded successfully.")
+      else
+        Logger.log(:error, "Failed to load data: #{response.code}")
+      end
+
+    rescue => e
+      Logger.log(:error, "Error on fetch_and_load for requested data: #{e}")
+    end
+  end
+end
+
---
+++ lib/log.rb
@@ -0,0 +1,7 @@
+
+class Logger
+  def self.log(type, message)
+    output = type == :error ? $stderr : $stdout
+    output.puts "#{Time.now.utc.iso8601} - #{type.upcase}: #{message}"
+  end
+end
